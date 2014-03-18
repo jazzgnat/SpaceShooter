15c90eb734155a802b08bfd1ef3d2de084a82135
iskoon <icskoon@gmail.com>
2014-03-17 20:34:40 -0400

    asteroids
    added asteroid creation script as well as asteroid asset

---
+++ Assets/AstroCreator.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AstroCreator : MonoBehaviour {
+
+	public GameObject asteroid;
+	private int count = 0; 
+
+	// Use this for initialization
+	void Start () {
+
+		while(count < 20){
+		Instantiate (asteroid, transform.position, transform.rotation);
+			count ++;
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/astroControl.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class astroControl : MonoBehaviour {
+
+
+	public bool collide = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnCollisionEnter(Collision myCol)
+	{
+		if(myCol.gameObject.name == "Cube")
+			collide = true;
+	}
+}
